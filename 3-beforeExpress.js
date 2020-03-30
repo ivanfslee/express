65ffa1e852b7ae0e7cbd02862165105caf72f79c
ivanfslee <ivanfslee@gmail.com>
2020-03-29 21:22:49 -0700

    Added notes from Pre-express video

---
+++ 3-beforeExpress.js
@@ -0,0 +1,27 @@
+//Pre-Express
+    //Typically associated with express:
+        //mongoDB
+        //React
+        //Angular
+        //node.js
+        //MySQL
+        //PostGreSQL
+    
+    //socket.io - real-time communication
+    
+    //Whenever you use any of those things mentioned above,
+        //express takes a backseat
+    
+    //Though those things above assume you're going to use express
+
+    //The course is made for you to get a good foundation of express
+        //before you use those other technologies that utilize express as well
+    
+    //However, we need to talk about 2 things before we talk about express
+        //1. HTTP - hypertext transfer protocol -> TCP/UDP 
+        //2. NodeJS webserver - we will make a webserver without using express
+            //Express is just a node module
+            //So, node can live without Express, but Express cannot live without node
+
+            //So, first, we try to see how node would do things without express
+                //That will help elucidate, how express works and why it is important
