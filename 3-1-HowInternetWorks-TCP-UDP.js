23489752371ad4678272cd570bb5f326089c9213
ivanfslee <ivanfslee@gmail.com>
2020-03-30 14:10:13 -0700

    Added notes from How the Internet Works video

--- 3-1-HowInternetWorks-TCP-UDP.js
+++ 3-1-HowInternetWorks-TCP-UDP.js
@@ -43,3 +43,29 @@
         //HTTP uses the transport layer. It uses TCP
 
 //The transport Layer creates 2^16 ports (~65000) on your computer
+    //When you are using port 3000 for a node app
+    //The reason you have port 3000 is that you are using one of the 65000 ports
+        //the transport layer creates
+    
+    //Ports analogy - 
+        //Hotel - a single building with a ton of numbered individual rooms
+        //If someone comes to the hotel in order to find a guest, they need to know the room number 
+        //and with the room number, they can actually find who they are looking for. 
+
+    //Application of a given machine initiates an HTTP request that will originate from arbitrary port 49742
+    //And it wants to talk to port 80 on another computer
+
+    //That request will get handed off to the transport layer
+        //where it will get wrapped up in a 'segment'
+        //Inside of segment is metadata and it will have destination port, which is port 80
+        //and it will have the source port - 49742
+
+    //Transport layer then hands it off to the network layer for further processing
+
+    //When it gets to the receiving machine, it will go through the process in reverse
+    //and eventually find the right port
+
+    //Two different types of transport layer protocols
+        //UDP
+
+        //TCP
