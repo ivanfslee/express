f2ccb3733936638ce73d27d9034d2a6a5f875738
ivanfslee <ivanfslee@gmail.com>
2020-03-30 00:10:41 -0700

    Added notes from How the Internet Works video

---
+++ 3-1-HowInternetWorks-TCP-UDP.js
@@ -0,0 +1,16 @@
+//Connecting to the 'cloud'
+    //The 'cloud' does not actually exist
+
+    //The 'cloud' is just a bunch of computers connected together
+
+    //The 'cloud' is someone elses computer
+
+    //AWS is a cloud service
+
+    //Azure, another cloud service
+
+    //GCP - google cloud platform
+
+    //These are all a bunch of computers in data centers in Ohio/Virginia/Oregon/ etc
+
+    //The 'cloud' is made up of alot of computers that don't belong to you.
