be62c7ac9a50efbbd83be7f461499673f1324c96
ivanfslee <ivanfslee@gmail.com>
2020-04-01 12:54:07 -0700

    Added notes from HTTP requests and how they work video

--- 3-2-HTTPRequestsAndHowTheyWork.js
+++ 3-2-HTTPRequestsAndHowTheyWork.js
@@ -41,3 +41,43 @@
                 //across the TCP and will send the response
         //HTTP is stateless
             //If you've done REST api's before, the ST at the end of REST stands for 'stateless'
+            //There is no dialogue. 
+            //That means, the machines only know about each other for as long as the connection is open
+            //As soon as the connection closes, everything is completely forgotten
+            //So if they need to talk again for any reason, they have to start
+            //completely over again.
+
+            //That is to say, its like the very first conversation.
+                //You have to tell me who you are and tell me what you want
+            
+            //Its not like a phone conversation, where you talk, and I remember what you just said.
+                //And the conversation flows, and I can recall what I said earlier
+                //HTTP doesn't work like this.
+
+                //Stateless means I only know about what you just said right now
+                //and I'm going to respond based on that.
+
+                //So, the requester gets one thing to say and the responder gets one response to it
+
+                //HTTP requests and response to UDEMY.com
+
+                    //1. client -> ISP -> (via TCP/IP) UDEMY servers
+                        // TCP says I would like to make a connection to your UDEMY servers
+
+                    //2. via TCP, the UDEMY servers will respond yes, I am okay with that
+
+                    //3. Data will go from client to UDEMY server
+                        //Some of that data will come in the form of an HTTP request
+                        //UDEMY server receives HTTP request from client
+                            //When that happens, that HTTP connection is terminated
+                            //TCP connection is still active and connected
+                    
+                    //4. Meanwhile, the client is still waiting on a response.
+                        //Client is still waiting for an HTTP response.
+                        //Server sends back an HTTP response
+                            //and that connection will close
+                    
+                    //5. TCP connection closes
+
+//HTTP message
+    //What does an HTTP message look like
