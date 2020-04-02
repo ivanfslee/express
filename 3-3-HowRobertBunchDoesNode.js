648674ea6aba459ec72eb47df872d3942517bab3
ivanfslee <ivanfslee@gmail.com>
2020-04-01 20:45:43 -0700

    Added notes to How Rover Bunch Does Node video

---
+++ 3-3-HowRobertBunchDoesNode.js
@@ -0,0 +1,19 @@
+//He uses VS code's integrated terminal
+//And uses as few plugins as possible
+
+//He'll run his node programs with nodemon
+    //to refresh any changes to his program
+
+    //run in terminal to install nodemon globally
+        //npm install -g nodemon
+
+//e.g. to run files with nodemon
+    //run the following in terminal:
+        //nodemon expressServer.js
+
+//run the following in terminal to see versions:
+    node -v //node js version
+
+    npm -v //npm version
+
+    nodemon -v //nodemon version
