62fe8732e85b6dbb8246627c6f5be00f4d43262c
ivanfslee <ivanfslee@gmail.com>
2020-04-05 00:14:37 -0700

    Added additional http paths to handle image and css file requests

--- 3-4-NodeHTTP-servers-101/nodeServerWithPaths.js
+++ 3-4-NodeHTTP-servers-101/nodeServerWithPaths.js
@@ -9,15 +9,25 @@ const http = require('http');
 const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    //console.log(req.url); //inside request object there is an 'url' property. This line will log the path the user requested relative to our root domain
+    console.log(`A request was made to: ${req.url}`); //inside request object there is an 'url' property. This line will log the path the user requested relative to our root domain
     
     if (req.url === '/') { //if requests url property is the root url
         //the user wants the homepage
         //we know, because the req object has '/' in the url
-        res.writeHead(200, {'content-type':'text/html'});
+        res.writeHead(200, {'content-type':'text/html'}); //'text/html' is a mime type, there are several hundred types
 
         const homePageHTML = fs.readFileSync('node.html'); //can use fs.readFile also
         res.write(homePageHTML); //under the hood, we are not serving the file itself, but the contents of the file written to the buffer
+        res.end();
+    } else if (req.url === '/node.png') {
+        res.writeHead(200, {'content-type':'image/png'}); //'image/png' is a mime type. There are several hundred types.
+        const image = fs.readFileSync('node.png'); //can use fs.readFile also
+        res.write(image); //under the hood, we are not serving the file itself, but the contents of the file written to the buffer
+        res.end();
+    } else if (req.url === '/styles.css') {
+        res.writeHead(200, {'content-type':'text/css'}); //'image/png' is a mime type. There are several hundred types.
+        const css = fs.readFileSync('styles.css'); //can use fs.readFile also
+        res.write(css); //under the hood, we are not serving the file itself, but the contents of the file written to the buffer
         res.end();
     } else {
         res.writeHead(404, {'content-type':'text/html'});
