0e43f83dcacbe084be75bb49da158b2385fb34ff
ivanfslee <ivanfslee@gmail.com>
2020-04-10 15:58:51 -0700

    Added helmet middleware and notes on helmet, express.urlencoded(), and express.json() middleware

--- 5-Express201/helmetAndOthers.js
+++ 5-Express201/helmetAndOthers.js
@@ -1,20 +1,30 @@
 const express = require('express');
 const app = express();
+const helmet = require('helmet');
+
+app.use(helmet());
+
 app.use(express.static('public'));
 //if you go inside express.static - you will find a function with req and res because it is a piece of middleware
 //Just like app.post(), app.get(), etc
 
 //This is middleware
 //we bring in express.json() like this:
-app.use(express.json());
+app.use(express.json()); //this is for data that is passed and has content-type/mime-type 'application/json or text/javascript'
 
 //This is middleware
+//If someone sends us data with a header and 'content-type/mime-type' of 'application/x-www-form-urlencoded; charset=UTF-8' we need this express.urlencoded middleware to parse out that data
 //we bring in express.urlencoded() like this:
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({extended:false})); //This line enables us to get the ajax.html JSON data ({name: 'Rob'}) that was sent from the ajax request
+
+//IMPORTANT NOTE: 
+    //app.use(express.json()) and app.use(express.urlencoded()) are two important middleware
+    //Together, they collect any type of submitted data, parse it and give it to you in json format
 
 //This defined route will handle the ajax request from ajax.html
 app.post('/ajax', (req, res) => {
-    console.log(req); //req.body contains the json data {name: 'Rob'} that was sent from ajax.html
+    console.log(req.body); //req.body contains the json data {name: 'Rob'} that was sent from ajax.html
+        //req.body is the mirror image of the object that was sent from ajax.html
     res.send('Testing'); //This will get sent back to the browser
 });
 
@@ -43,12 +53,19 @@ app.post('/ajax', (req, res) => {
     //we can bring in options like {extended:false}
         //app.use(express.urlencoded({extended:false}));
 
-    //9:00
+    //
 app.listen(3000);
 console.log('Server is listening on port 3000');
+ 
+//Helmet - express middleware
+    //It sets the HTTP headers right up front and protects from a bunch of well-known vulnerabilities
 
 //Request object properties that we have explored so far:
     //req.body - contains the json data {name: "Rob"} from ajax request initiated in ajax.html
-    //req.headers
+        //req.body is called 'body' because it is based on the 'body-parser' module
+        //req.body is the mirror of whatever data was sent from the ajax.html ajax request
+        //req.body only exists because of the express.json() middleware creates the empty object {}
+        //req.body is filled up with data from express.urlencoded({extended:false}) middleware (line 18)
+    //req.headers - this will show that the content-type/mime-type is 'application/x-www-form-urlencoded; charset=UTF-8'
     //req.route
     //Google - request object deconstructed
