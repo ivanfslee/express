84686437be351b0619efae30a0b83d2c00a9f990
ivanfslee <ivanfslee@gmail.com>
2020-04-13 01:23:06 -0700

    Added notes of API vs server-side rendering

---
+++ 5-Express201/apiVsServerSideRendering.js
@@ -0,0 +1,35 @@
+//Choosing res.render() vs. res.json() for our response
+    //Two ways to go with express
+    //Though, they are not mutually exclusive, you CAN use them together
+    //We are treating them separately to show how they care conceptually different
+
+//Server-side rendering vs. Building an API
+
+//Go to Myspace in 2003
+    //You, the client make a request to Myspace servers on port 80
+    //Myspace gets your request
+    //Myspace sends a response of HTML, JS, CSS
+    //Your browser can mainly read 3 things - HTML, JavaScript, CSS
+
+    //The question is what goes on inside the MySpace servers?
+    
+//Inside the Server
+//4:34
+//FE UI - Front-end UI - Reach/Vue
+//PL - C/C++/Java/Python/Ruby/PHP
+//Database Layer // DB - database - SQL/DynamoDB/MongoDB
+//Web Server layer // WS - Apache or IIS or NGINX or NodeJS or Web Sphere
+//Bottom layer of the stack is the Operating System //OS - linux or windows server or UNIX
+
+//Port 80 - created by the transport layer
+
+
+
+
+//res.render
+    //server-side rendering/session cookies
+    //User always has to come to the server for everything
+    //because the server contains everything
+
+//res.json
+    //API/JSON needs
