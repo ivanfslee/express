a5694d4e47ecdb06d31c1d57c3a2a96db092ac72
ivanfslee <ivanfslee@gmail.com>
2020-04-18 00:47:37 -0700

    Added package.json and added /process_login route in loginSite.js

--- 6-Express301/loginSite.js
+++ 6-Express301/loginSite.js
@@ -13,9 +13,21 @@ app.use(express.urlencoded({extended:false}));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-app.get('/', (req, res, next) => {
+app.get('/login', (req, res, next) => {
     res.render('login');
+});
+
+app.post('/process_login', (req, res, next) => {
+    res.render('welcome');
 });
 
 app.listen(3000);
 console.log('Server is listening on port 3000');
+
+//Git: Revert to a Previous Commit
+    // https://stackabuse.com/git-revert-to-a-previous-commit/
+    //Get hash-or-ref from github
+        //git reset --hard <hash-or-ref>
+
+//Create 'package.json' file
+    //npm init
