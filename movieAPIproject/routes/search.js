46e9e21a9657a5eb34efe4ecd8a265b2406c49c8
ivanfslee <ivanfslee@gmail.com>
2020-05-09 18:05:24 -0700

    Added middleware to be run only for search router. Middleware checks that user provided a query string to search for

--- movieAPIproject/routes/search.js
+++ movieAPIproject/routes/search.js
@@ -4,12 +4,21 @@ var router = express.Router();
 const movies = require('../data/movies');
 const people = require('../data/people');
 
-/* GET search page. */
-// '/' route refers to '/search/...' route
-router.get('/', function(req, res, next) {
-    console.log('search router works');
-    res.render('index', { title: 'Express' });
-});
+//This middleware will check if the user
+    // provided a query string
+function queryGiven(req, res, next) {
+    console.log('req.query is: ', req.query);
+    if (!req.query.q) {
+        res.json({msg: 'query must be provided'})
+    } else {
+        next();
+    }
+}
+
+//We apply the middleware to every route
+    //in this router, using router.use()
+router.use(queryGiven);
+
 
 //The 2 defined routes in this router:
     //Route: GET /search/movie
@@ -18,21 +27,31 @@ router.get('/', function(req, res, next) {
 
 // GET route to search for movie - /search/movie
 router.get('/movie', (req, res, next) => {
-    const searchTerm = req.query.q;
+    //Movie API says that the search term should be URI encoded
+    //URI = uniform resource identifier
+    //This is to handle different languages and spaces in the search term
+    //You encode the search term to normalize it
+    //For example, ' ' spaces in the search term will show up as '%20' in the URL
+
+    const searchTerm = encodeURI(req.query.q).toLowerCase();
     const results = movies.filter(movie => {
-        return movie.title.includes(searchTerm);
+        return movie.title.toLowerCase().includes(searchTerm) || movie.overview.toLowerCase().includes(searchTerm);
     });
     res.json({results});
 });
 
 // GET route to search for person - /search/person
 router.get('/person', (req, res, next) => {
-    const searchTerm = req.query.q;
+    //Movie API says that the search term should be URI encoded
+    //URI = uniform resource identifier
+    //This is to handle different languages and spaces in the search term
+    //You encode the search term to normalize it
+    //For example, ' ' spaces in the search term will show up as '%20' in the URL
+    const searchTerm = encodeURI(req.query.q);
     const results = people.filter(person => {
-        return person.name.includes(searchTerm);
+        return person.name.toLowerCase().includes(searchTerm);
     });
-
-
     res.json({results});
 });
+
 module.exports = router;
