f31df19a49fed31bdf4c4e681ce569e4f2de61ba
ivanfslee <ivanfslee@gmail.com>
2020-05-14 00:09:27 -0700

    Added clarifying notes on TCP vs UDP.

--- 3-prereqExpress/3-1-HowInternetWorks-TCP-UDP.js
+++ 3-prereqExpress/3-1-HowInternetWorks-TCP-UDP.js
@@ -49,51 +49,55 @@
     // Application Layer - As developers, we will mostly be working here. HTTP, FTP, SSH, SMTP for email (all these are the various protocols used in this layer)
     // Transport Layer - either UDP or TCP
         //The transport layer and network layer, together form the Internet Protocol suite or TCP/IP
+        //Two main types of transport layer protocols - TCP and UDP 
     // Network Layer (sometimes called the internet layer) - IP 
     // Link Layer - wifi/ethernet connection (a hardware layer)
     // Physical layer - the physical cables connecting stuff together (a hardware layer)
 
     //As developers, we typically deal with Application layer the most, and sometimes Transport and Network Layer
         //Express only handles HTTP requests
-        //HTTP uses the transport layer. It uses TCP
+        //HTTP is used in the application layer.
+        //HTTP utilizes the transport layer. HTTP uses TCP.
 
 //The transport Layer creates 2^16 ports (~65000) on your computer
     //When you are using port 3000 for a node app
     //The reason you have port 3000 is that you are using one of the 65000 ports
-        //the transport layer creates
+        //the transport layer creates 65000 ports on your computer
     
     //Ports analogy - 
         //Hotel - a single building with a ton of numbered individual rooms
         //If someone comes to the hotel in order to find a guest, they need to know the room number 
         //and with the room number, they can actually find who they are looking for. 
 
-    //Application of a given machine initiates an HTTP request that will originate from arbitrary port 49742
-    //And it wants to talk to port 80 on another computer
+    //Application of a given machine initiates an HTTP request (or some kind of network request) that will originate from arbitrary port 49742
+        //And it wants to talk to port 80 on another computer
 
     //That request will get handed off to the transport layer
         //where it will get wrapped up in a 'segment'
-        //Inside of segment is metadata and it will have destination port, which is port 80
+        //Inside of 'segment' is metadata and it will have destination port, which is port 80
         //and it will have the source port - 49742
 
     //Transport layer then hands it off to the network layer for further processing
 
     //When it gets to the receiving machine, it will go through the process in reverse
-    //and eventually find the right port
+        //and eventually find the right port
 
     //Two different types of transport layer protocols
         //UDP
             //User Datagram Protocol
             //HTTP uses TCP and not UDP
             //UDP is incredibly light weight 
-                //8 bytes
+                //8 bytes for the header
+                //UDP has little overhead
             //UDP is connectionless
                 //You don't have to wait for the connection to be established
                 //Client can just start talking to server right away and send data
+                //Even if the other computer doesn't want to hear from you.
             //Consistency
                 //UDP will send data no matter what
                 //Packet loss? If there is packet loss, UDP will keep on sending data
                 //What if network is congested? UDP doesn't care. It will just keep sending packets and make network more congested
-                //Packets out of order? UDP doesn't care.
+                //Packets out of order? UDP doesn't care. They will show up out of order on the other side then.
             //The upside to UDP is that it is very FAST
             //Headers are incredibly small
             //You don't have to bother to set up a connection to start. You just send data.
@@ -108,6 +112,9 @@
             
             //Bottom Line:
                 //UDP is fast but incredibly unreliable
+                //UDP could be sending data, but there might not even be anyone on the other side to receive
+                    //Or Server refuses to get the data for some reason
+                    //or server disappears before data ges there
                 //HTTP is not based on UDP, so Express does not have the faults of UDP
         //TCP
             //Transmission Control Protocol
@@ -119,7 +126,7 @@
                 //3-way handshake
                     //1. Client asks server to initiate connection
                     //2. Server says yes or no
-                    //3. Data starts going from client to server
+                    //3. If yes, Data starts going from client to server
 
             //Because of 3-way handshake, TCP is reliable
                 //UDP just sends data, even if there is no server there. Or server disappears before packet arrives there or server refuses the packet
@@ -127,6 +134,8 @@
             //TCP also has delivery acknowledgement of data. 
                 //Every time data comes through to server, the server will let the client know that the server got the clients data and vice versa
                 //TCP gets acknowledgement that the data got received.
+                //The receiving server basically says 'I got the data'.
+                    //Analogous to certified mail/mail with receipt at the post office
             
             //TCP has retransmission of data.
                 //UDP may not know whether data was received or not because there isn't data acknowledgement.
